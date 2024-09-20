3c8cc2f66d9789a81358ef851fda6c4b97d934d9
Niketha Nethaji <nnethaji@ufl.edu>
2024-09-20 10:02:12 +0530

    initial routing with blueprints

---
+++ featherly-dev/featherly/blueprints/page/views.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, render_template
+
+page = Blueprint('page', __name__, template_folder='templates')
+
+@page.route('/')
+def home():
+    # landing page
+    return render_template('page/home.html')
+    # return "Hello Worldddd"
+
+@page.route('/signup')
+def signup():
+    return render_template('page/signup.html')
+
+@page.route('/signin')
+def signin():
+    return render_template('page/signin.html')
