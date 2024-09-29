e85d2476598753cf4590253f89a5490cbfaa49d6
Niketha Nethaji <nnethaji@ufl.edu>
2024-09-29 20:58:12 +0530

    page.tsx

---
+++ nextjs-flask/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Feed from '@/components/social media feed/index'
+export default function Home() {
+  return (
+    <>
+      <Feed/> 
+    {/* <p> hello social media feed</p> */}
+    </>
+  )
+}
