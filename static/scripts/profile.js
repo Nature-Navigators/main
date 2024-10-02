cae04bc650e5042a77cc88ca468bfb065400df3e
Alaine Spade <spadea@ufl.edu>
2024-10-02 16:23:30 -0400

    edit profile popup

--- static/scripts/profile.js
+++ static/scripts/profile.js
@@ -1,4 +1,41 @@
+// ============================================ EVENTS ===================================================
 
+//set up profile button events
+var profileButton = document.querySelector(".profile_pic");
+var editProfileText = profileButton.getElementsByTagName("p")[0];
+
+//mouse events
+profileButton.addEventListener("mouseover", ()=> { editProfileText.style.visibility = 'visible'; });
+profileButton.addEventListener("mouseout", () => {editProfileText.style.visibility = 'hidden';});
+
+//click events
+profileButton.addEventListener("click", () => {
+    setDefaultEditProfile();
+    document.getElementById("edit_profile_popup").style.visibility = 'visible';
+    grayOut(true);
+})
+
+
+// ======================================== FUNCTIONS =================================================
+
+//set default profile edit popup values
+function setDefaultEditProfile() {
+    var profileForm = document.getElementById("edit_profile_form");
+    var inputs = profileForm.getElementsByTagName("input");
+    inputs[0].value = document.getElementById("name").innerHTML;
+    inputs[1].value = document.getElementById("title").innerHTML;
+
+    var bio = profileForm.getElementsByTagName("textarea")[0];
+    bio.value = document.querySelector(".content").getElementsByTagName("p")[0].innerText;
+}
+
+
+
+function hideProfilePopup() {
+    document.getElementById("edit_profile_popup").style.visibility = 'hidden';
+    grayOut(false);
+
+}
 
 function showGallery() {
     document.getElementById('gallery').style.display = 'grid';
