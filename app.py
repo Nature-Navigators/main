2aa67ecef3dbfebdfefed054b2021667f38a89bd
allison <allison.reilly@zoho.com>
2024-10-13 20:36:21 -0400

    Added birds to rectangles list

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import folium
 import os
 import xyzservices.providers as xyz #Can use this to change map type
 from temp_data import *
+from urllib.parse import quote
 # from markupsafe import Markup
 
 app = Flask(__name__)
@@ -24,13 +25,15 @@ def update_location():
     data = request.get_json()
     latitude = data['latitude']
     longitude = data['longitude']
+    
+    page = data.get('page', 1)
+    page_size = 3
+    
     birds_near_user = getRecentBirds(latitude, longitude)
 
-    #Create Folium map at user's location
     m = folium.Map(location=[latitude, longitude], zoom_start=13)
     folium.Marker([latitude, longitude], tooltip='Your Location').add_to(m)
 
-    #Add markers for each bird observation
     for bird in birds_near_user:
         bird_name = bird.get('comName')
         bird_lat = bird.get('lat')
@@ -41,11 +44,30 @@ def update_location():
             icon=folium.Icon(color='red')
         ).add_to(m)
 
-    #Get HTML of map
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    paginated_birds = birds_near_user[start_index:end_index]
+
+    bird_data = []
+    
+    for index, bird in enumerate(paginated_birds):
+        bird_name = bird.get('comName')
+
+        formatted_bird_name = formatBirdName(bird_name)
+        image_url = getWikipediaImage(formatted_bird_name)
+        description = f"{bird_name} is spotted near your location."
+
+        bird_data.append({
+            'imageUrl': image_url,
+            'title': bird_name,
+            'description': description,
+            'url': f'/bird/{quote(bird_name)}'
+        })
+
+    # Get HTML of map
     map_html = m._repr_html_()
 
-    #return HTML in JSON format
-    return jsonify(mapHtml=map_html)
+    return jsonify(mapHtml=map_html, birdData=bird_data)
 
 def getRecentBirds(latitude, longitude):
     headers = {
@@ -61,6 +83,46 @@ def getRecentBirds(latitude, longitude):
         return response.json()
     else:
         return []
+    
+def formatBirdName(bird_name):
+    bird_name = bird_name.replace(' ', '_')
+
+    parts = bird_name.split('_')
+
+    formatted_name = parts[0]
+
+    if len(parts) > 1:
+        formatted_name += '_' + '_'.join(part.lower() for part in parts[1:])
+
+    return formatted_name
+
+def getWikipediaImage(bird_name):
+    formatted_bird_name = formatBirdName(bird_name)
+    search_url = f'https://en.wikipedia.org/w/api.php?action=query&titles={formatted_bird_name}&prop=pageimages&format=json&pithumbsize=100'
+    response = requests.get(search_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        pages = data.get('query', {}).get('pages', {})
+        for page_id, page in pages.items():
+            image_url = page.get('thumbnail', {}).get('source')
+            if image_url:
+                return image_url
+    else:
+        print(f"Failed to fetch data from Wikipedia. Status code: {response.status_code}")
+
+    return None
+
+def get_bird_info(bird_name):
+    return {
+        'imageUrl': getWikipediaImage(bird_name),
+        'title': bird_name
+    }
+
+@app.route('/bird/<bird_name>')
+def bird_page(bird_name):
+    bird_info = get_bird_info(bird_name)
+    return render_template('bird.html', bird=bird_info)
 
 @app.route('/signin')
 def signin():
