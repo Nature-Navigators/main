b2c183ee96820d24d613a9ff7f6f16c647d3e10d
Alaine Spade <spadea@ufl.edu>
2024-11-14 15:37:24 -0500

    tabs on follower popup

--- static/scripts/follower_popup.js
+++ static/scripts/follower_popup.js
@@ -2,3 +2,31 @@ function hideFollowerPopup() {
 
     document.getElementById("follower_popup").style.visibility = 'hidden';
 }
+
+function clickFollowerFollowing(following)
+{
+    let followersHeader = document.getElementById("followersHeader");
+    let followingHeader = document.getElementById("followingHeader");
+
+    let followingList = document.getElementById("followingList");
+    let followersList = document.getElementById("followersList");
+
+    if(following)
+    {
+        followersHeader.classList.remove("active");
+        followingHeader.classList.add("active");
+
+        followingList.style.display = "block";
+        followersList.style.display = "none";
+    }
+    else
+    {
+        followersHeader.classList.add("active");
+        followingHeader.classList.remove("active");
+
+        followingList.style.display = "none";
+        followersList.style.display = "block";
+
+    }
+
+}
